90be72f2fe3515ca975f8c8a74a07acd4c28edea
Chip Cuccio <chipster@gnomeslackbuild.org>
2005-02-13 23:35:04 +0000

    blah

--- web/common/header_inc.php
+++ web/common/header_inc.php
@@ -6,8 +6,7 @@
     $Id$
 
 */
-header("Content-Type: text/html;charset=utf-8\n"
-      ."Vary: Accept\n");
+header("Content-Type: text/html;charset=utf-8");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
