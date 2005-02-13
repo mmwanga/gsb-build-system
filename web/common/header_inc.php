ac49fc8b46a6720310b8d1d17435d79b26101ba1
Chip Cuccio <chipster@gnomeslackbuild.org>
2005-02-13 23:22:44 +0000

    had to omit XML doctyp declaration due to SF.net's mime-type encoding ingnorance :(

--- web/common/header_inc.php
+++ web/common/header_inc.php
@@ -6,7 +6,8 @@
     $Id$
 
 */
-print('<?xml version="1.0" encoding="utf-8"?>' . "\n");
+header("Content-Type: text/html;charset=utf-8\n"
+      ."Vary: Accept\n");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -19,7 +20,7 @@ print('<?xml version="1.0" encoding="utf-8"?>' . "\n");
 // -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="MSSmartTagsPreventParsing" content="TRUE" />
   <meta http-equiv="MSTHEMECOMPATIBLE" content="no" />
   <link rel="stylesheet" type="text/css" href="./css/style.css"
