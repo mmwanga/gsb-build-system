d200e210f3be2c72961bf4591839922af3643945
Chip Cuccio <chipster@gnomeslackbuild.org>
2005-02-13 23:14:53 +0000

    OK - sf.net sucks. had to hack the 'content-type' header due to SF's bad apache mime encoding types/maps.

--- web/common/header_inc.php
+++ web/common/header_inc.php
@@ -6,7 +6,7 @@
     $Id$
 
 */
-print('<?xml version="1.0" encoding="utf-8"?>' . "\n");
+print('<?xml version="1.0" encoding="iso-8859-1"?>' . "\n");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -17,8 +17,11 @@ print('<?xml version="1.0" encoding="utf-8"?>' . "\n");
     $Id$
 
 // -->
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
+  <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+  <meta name="MSSmartTagsPreventParsing" content="TRUE" />
+  <meta http-equiv="MSTHEMECOMPATIBLE" content="no" />
   <link rel="stylesheet" type="text/css" href="./css/style.css"
   media="screen" title="Default Style" />
   <link rel="alternate stylesheet" type="text/css" href=
