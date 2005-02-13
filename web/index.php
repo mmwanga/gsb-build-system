231856ee078280ebf68d559f2c317fc29b8ffcae
Chip Cuccio <chipster@gnomeslackbuild.org>
2005-02-13 19:39:22 +0000

    Added TODO Fixed index.php linebreak

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
-
 /*
+
     The index.php "brain" PHP script
     Chip Cuccio <chipster@norlug.org>
     $Id$
@@ -11,5 +11,4 @@ include('common/header_inc.php');
 include('common/sidebar_inc.php');
 include('common/main-index-body_inc.php');
 include('common/footer_inc.php');
-
 ?>
