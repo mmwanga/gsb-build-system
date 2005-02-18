1a191feb052f9aa6355d60748dd9108f82da06c6
Alak Trakru <freerock@gmail.com>
2005-02-18 22:15:42 +0000

    make sure gdm.conf isn't overwritten by new package

--- src/gnome/desktop/gdm/doinst.sh
+++ src/gnome/desktop/gdm/doinst.sh
@@ -1,3 +1,18 @@
 if [ -x /usr/bin/scrollkeeper-update ]; then
   /usr/bin/scrollkeeper-update -p /var/lib/scrollkeeper 1> /dev/null 2> /dev/null
 fi
+
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/X11/gdm/gdm.conf.new
+
