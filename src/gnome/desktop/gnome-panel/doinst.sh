59976173061db3440be327d3af20e9f37f3e93cd
Alak Trakru <freerock@gmail.com>
2005-02-20 22:02:00 +0000

    make doinst.sh for gnome-panel $ROOT safe

--- src/gnome/desktop/gnome-panel/doinst.sh
+++ src/gnome/desktop/gnome-panel/doinst.sh
@@ -1,4 +1,6 @@
-for i in /etc/gconf/schemas/*.entries
+GCONF_SOURCE=`gconftool-2 --get-default-source| sed 's#/etc#etc#'`
+
+for i in etc/gconf/schemas/*.entries
 do
-	gconftool-2 --config-source=`gconftool-2 --get-default-source` --load $i
+	gconftool-2 --config-source=$GCONF_SOURCE --load $i
 done
