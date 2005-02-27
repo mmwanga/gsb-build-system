add1ca28b9611c711dcee19c1fb60e016db2398a
Alak Trakru <freerock@gmail.com>
2005-02-27 21:15:40 +0000

    another fix for gnome-panel issues, hopefully this fixes it

--- src/gnome/desktop/gnome-panel/doinst.sh
+++ src/gnome/desktop/gnome-panel/doinst.sh
@@ -3,6 +3,7 @@ GCONF_SOURCE=`gconftool-2 --get-default-source| sed 's#/etc#etc#'`
 for i in etc/gconf/schemas/*.entries
 do
 	gconftool-2 --config-source=$GCONF_SOURCE --load $i
+	gconftool-2 --config-source=$GCONF_SOURCE --load $i /apps/panel
 done
 if [ -x usr/bin/scrollkeeper-update ]; then
   usr/bin/scrollkeeper-update -p var/lib/scrollkeeper 1> /dev/null 2> /dev/null
