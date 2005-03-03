1e0e1037fe75ca5503f4ba3a31cc745cf99fe491
Alak Trakru <freerock@gmail.com>
2005-03-03 20:49:36 +0000

    add rc.howl to rc.local from howl's doinst.sh

--- src/gnome/desktop_reqs/howl/doinst.sh
+++ src/gnome/desktop_reqs/howl/doinst.sh
@@ -12,3 +12,22 @@ config() {
 
 config etc/rc.d/rc.howl.new
 
+# if rc.local doesn't exist, create it
+if [ ! -e etc/rc.d/rc.local ]; then
+	echo "#!/bin/sh" > etc/rc.d/rc.local
+	chmod 755 etc/rc.d/rc.local
+fi
+	
+# if rc.howl is executable, run it on startup
+run=`grep ". /etc/rc.d/rc.howl" etc/rc.d/rc.local`
+if [[ "${run}" == "" ]]; then	
+cat << EOF >> etc/rc.d/rc.local
+
+# To disable howl, chmod rc.howl to 644
+if [ -x /etc/rc.d/rc.howl ]; then
+  echo "Starting Howl mDNS daemons"
+  . /etc/rc.d/rc.howl start
+fi
+EOF
+fi
+
