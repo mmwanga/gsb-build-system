aab48e5430ba06c216ed289383468e90d21bd912
Chip Cuccio <chipster@gnomeslackbuild.org>
2005-03-05 04:25:31 +0000

    * Added more of MMYoungs screenshots. * Added "make-thumbs.sh" script to easily create thumbnails.

---
+++ web/screenies/make-thumbs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# $Id:$
+
+clear
+
+echo "removing existing thumbs ..."
+echo ""
+rm -rf thumb*
+sleep 1
+
+echo ""
+echo "generating thumbnails from regular images (this may take a minute) ..."
+echo ""
+
+for i in $( ls *.png ); do
+    echo "      Generating thumb-$i"
+    convert -geometry 100x100 $i thumb-$i
+done
+
+echo ""
+echo "DONE!"
+echo ""
+echo ""
+
+exit
