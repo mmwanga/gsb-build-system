ca919714b0b1ed961117766c66a218f66139e371
Alak Trakru <freerock@gmail.com>
2005-03-08 15:15:10 +0000

    templates for scripts to make releases

---
+++ bin/misc/frg_release_make.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+# make a release iso for FRG 
+# append frg to all package BUILD variables 
+
+
---
+++ bin/misc/gsb_release_make.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+# make a release tarball for GSB
+# append gsb to all BUILD variables in the SlackBuild files
+
+
