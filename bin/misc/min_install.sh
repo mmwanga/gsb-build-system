f063769d3287ff5c8b67b72fc0cbad99eb524b69
Alak Trakru <freerock@gmail.com>
2005-03-11 07:10:50 +0000

    update min_install.sh for GNOME-2.10

--- bin/misc/min_install.sh
+++ bin/misc/min_install.sh
@@ -2,35 +2,22 @@
 
 FRGROOT=`dirname $0`
 
-# Install requirements
-(
-  cd $FRGROOT/requirements
-  upgradepkg --install-new dbus*.tgz
-  upgradepkg --install-new hal*.tgz
-
-  upgradepkg --install-new \
-      libcap*.tgz \
-      libcroco*.tgz \
-      libgsf*.tgz \
-      openh323*.tgz \
-      pwlib*.tgz \
-      howl*.tgz \
-      openldap*.tgz \
-      cyrus-sasl*.tgz \
-      hicolor-icon-theme*.tgz
-)
-
-# Install Gstreamer and media libs
-(
-  cd $FRGROOT/gstreamer
-  upgradepkg --install-new libs/*.tgz
-  upgradepkg --install-new gstreamer*.tgz
-  upgradepkg --install-new gst-plugins*.tgz
-)
 
 # Install Platform
 (
   cd $FRGROOT/platform
+  upgradepkg --install-new *.tgz
+  upgradepkg --reinstall shared-mime-info*.tgz
+)
+
+# Install Desktop requirements
+(
+  cd $FRGROOT/desktop_reqs
+  upgradepkg --install-new dbus*.tgz
+  upgradepkg --install-new hal*.tgz
+  upgradepkg --install-new cyrus-sasl*.tgz
+  upgradepkg --install-new openldap*.tgz
+
   upgradepkg --install-new *.tgz
 )
 
