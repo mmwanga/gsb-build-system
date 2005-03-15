b59327f88b76e281ddf21207bfe2aa5cf43f3f4a
Chip Cuccio <chipster@gnomeslackbuild.org>
2005-03-15 20:42:18 +0000

    improved thumbnail script

--- web/make-thumbs.sh
+++ web/make-thumbs.sh
@@ -7,7 +7,10 @@ clear
 echo "removing existing thumbs ..."
 echo ""
 cd ./screenies
-rm -rf thumb*
+for i in $( ls thumb-* ); do
+    rm -f $i
+    echo "      deleted '$i'"
+done
 sleep 1
 
 echo ""
@@ -15,9 +18,13 @@ echo "generating thumbnails from regular images (this may take a minute) ..."
 echo ""
 
 for i in $( ls *.png ); do
-    echo "      Generating thumb-$i"
+    echo "      Generating 'thumb-$i'"
     convert -geometry 100x100 $i thumb-$i
 done
+
+echo ""
+echo "Adding screenshots and thumbnails into SVN"
+svn --quiet add *.png
 
 echo ""
 echo "DONE!"
