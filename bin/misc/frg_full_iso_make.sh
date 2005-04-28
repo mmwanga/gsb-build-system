099226d9e17663a0de004349177d79d906f4fb40
Alak Trakru <freerock@gmail.com>
2005-04-28 03:07:05 +0000

    frg iso scripts: Update version numbers and shorten variable name

--- bin/misc/frg_full_iso_make.sh
+++ bin/misc/frg_full_iso_make.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-FRG=0.2.0
-GNOME=2.10.0
+FRG=0.2.1
+GNOME=2.10.1
 
 ISO_NAME="frg-${FRG}-${GNOME}.iso"
 
-FRG_FTP_ROOT=/home/storage/pub/frg/
+FRG_ROOT=/home/storage/pub/frg/
 
 /usr/bin/mkisofs \
     -R -J -o ${ISO_NAME} \
     -V "FRG ${FRG}" \
     -A "Freerock GNOME ${FRG}" \
-    $FRG_FTP_ROOT/frg-$FRG/
+    $FRG_ROOT/frg-$FRG/
 
 
 /usr/bin/md5sum \
--- bin/misc/frg_min_iso_make.sh
+++ bin/misc/frg_min_iso_make.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
-FRG=0.2.0
-GNOME=2.10.0
+FRG=0.2.1
+GNOME=2.10.1
 
 ISO_NAME="frg-min-${FRG}-${GNOME}.iso"
 
-FRG_FTP_ROOT=/home/storage/pub/frg/
+FRG_ROOT=/home/storage/pub/frg/
 
 /usr/bin/mkisofs \
     -R -J -o ${ISO_NAME} \
     -V "FRG ${FRG} Minimum" \
     -A "Freerock GNOME ${FRG}" \
-    -x $FRG_FTP_ROOT/frg-$FRG/bindings \
-    -x $FRG_FTP_ROOT/frg-$FRG/office \
-    -x $FRG_FTP_ROOT/frg-$FRG/other \
-    -x $FRG_FTP_ROOT/frg-$FRG/testing \
-    -x $FRG_FTP_ROOT/frg-$FRG/themes \
-    -x $FRG_FTP_ROOT/frg-$FRG/updates \
-    -x $FRG_FTP_ROOT/frg-$FRG/patches \
-    -x $FRG_FTP_ROOT/frg-$FRG/frg_full_dups_install.sh \
-    -x $FRG_FTP_ROOT/frg-$FRG/frg_full_install.sh \
-    -m $FRG_FTP_ROOT/frg-$FRG/dups/'gnumeric*' \
-    -m $FRG_FTP_ROOT/frg-$FRG/dups/'drivel*' \
-    -m $FRG_FTP_ROOT/frg-$FRG/dups/'gimp*' \
-    -m $FRG_FTP_ROOT/frg-$FRG/alternatives/'drivel*' \
-    -m $FRG_FTP_ROOT/frg-$FRG/alternatives/'gimp*' \
-    -m $FRG_FTP_ROOT/frg-$FRG/alternatives/'sylpheed*' \
-    $FRG_FTP_ROOT/frg-$FRG/
+    -x $FRG_ROOT/frg-$FRG/bindings \
+    -x $FRG_ROOT/frg-$FRG/office \
+    -x $FRG_ROOT/frg-$FRG/other \
+    -x $FRG_ROOT/frg-$FRG/testing \
+    -x $FRG_ROOT/frg-$FRG/themes \
+    -x $FRG_ROOT/frg-$FRG/updates \
+    -x $FRG_ROOT/frg-$FRG/patches \
+    -x $FRG_ROOT/frg-$FRG/frg_full_dups_install.sh \
+    -x $FRG_ROOT/frg-$FRG/frg_full_install.sh \
+    -m $FRG_ROOT/frg-$FRG/dups/'gnumeric*' \
+    -m $FRG_ROOT/frg-$FRG/dups/'drivel*' \
+    -m $FRG_ROOT/frg-$FRG/dups/'gimp*' \
+    -m $FRG_ROOT/frg-$FRG/alternatives/'drivel*' \
+    -m $FRG_ROOT/frg-$FRG/alternatives/'gimp*' \
+    -m $FRG_ROOT/frg-$FRG/alternatives/'sylpheed*' \
+    $FRG_ROOT/frg-$FRG/
 
 
 /usr/bin/md5sum \
