1c54ba2729f2395abe41f3c9a10c6604ae2b8f28
Chip Cuccio <chipster@gnomeslackbuild.org>
2005-06-22 21:06:25 +0000

    Changed DTD to the elite set of XHTML 1.1 plus MathML 2.0 plus SVG 1.1 :-)

--- web/common/header_inc.php
+++ web/common/header_inc.php
@@ -9,8 +9,9 @@
 header("Content-Type: text/html;charset=utf-8");
 print("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC
+  "-//W3C//DTD XHTML 1.1 plus MathML 2.0 plus SVG 1.1//EN"
+  "http://www.w3.org/2002/04/xhtml-math-svg/xhtml-math-svg.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
