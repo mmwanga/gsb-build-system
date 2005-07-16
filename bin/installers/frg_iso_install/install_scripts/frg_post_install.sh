8339f32bd51ec5cdebe9d9fa0b13a46c54de9e1d
Alak Trakru <freerock@gmail.com>
2005-07-16 20:41:40 +0000

    frg_iso_install: use relative paths in frg_post_install.sh for people who install to a different $ROOT, thanks to Darren Austin for the suggestion

--- bin/installers/frg_iso_install/install_scripts/frg_post_install.sh
+++ bin/installers/frg_iso_install/install_scripts/frg_post_install.sh
@@ -3,8 +3,27 @@
 # script to be run post frg install
 #
 
-/usr/bin/update-mime-database /usr/share/mime
-/usr/bin/update-desktop-database
+CWD=`pwd`
+
+if [ $ROOT != "" ]; then
+  cd $ROOT
+else
+  cd /
+fi
+
+usr/bin/gdk-pixbuf-query-loaders > etc/gtk-2.0/gdk-pixbuf.loaders
+usr/bin/gtk-query-immodules-2.0 > etc/gtk-2.0/gtk.immodules
+
+for i in `bin/ls -1 usr/share/icons`
+do
+       if [ -e usr/share/icons/$i/icon-theme.cache ]; then
+               rm -f usr/share/icons/$i/icon-theme.cache
+       fi
+       usr/bin/gtk-update-icon-cache usr/share/icons/$i
+done
+
+usr/bin/update-mime-database usr/share/mime
+usr/bin/update-desktop-database
 
 sh "$FRGROOT"/install_scripts/frg_check_reqs.sh
 
@@ -27,11 +46,11 @@ Do you want to enable the rc.d scripts for Freerock GNOME packages?\n"
 fi
 
 
-if [ "RCD" = "on" ]; then
+if [ "$RCD" = "on" ]; then
     rcd_selection=1
 fi
 
-if [ "RCD" = "off" ]; then
+if [ "$RCD" = "off" ]; then
     rcd_selection=2
 fi
 
@@ -39,9 +58,9 @@ case "$rcd_selection" in
 '1')
 	echo ""
 	echo "Enabling D-BUS"
-	chmod +x /etc/rc.d/rc.messagebus
+	chmod +x etc/rc.d/rc.messagebus
 	echo "Enabling Hal"
-	chmod +x /etc/rc.d/rc.hal
+	chmod +x etc/rc.d/rc.hal
 	echo ""
 	echo "rc.howl, rc.saslauthd and rc.openldap are in also in"
 	echo "etc/rc.d for your consideration"
@@ -56,3 +75,6 @@ case "$rcd_selection" in
 	echo "No rc.d scripts enabled"
 	echo ""
 esac
+
+cd "$CWD"
+
