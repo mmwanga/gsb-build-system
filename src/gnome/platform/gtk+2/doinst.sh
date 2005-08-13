36b74611edcc878c6cb11701eae58cab46a07ae1
Alak Trakru <freerock@gmail.com>
2005-08-13 22:30:52 +0000

    gsb: ARCH_CONFIGURE and x86_64 patches from Jason Woodward

--- src/gnome/platform/gtk+2/doinst.sh
+++ src/gnome/platform/gtk+2/doinst.sh
@@ -1,3 +1,3 @@
 
 usr/bin/gdk-pixbuf-query-loaders > etc/gtk-2.0/gdk-pixbuf.loaders
-usr/bin/gtk-query-immodules-2.0 > etc/gtk-2.0/gtk.immodules
+#no longer there usr/bin/gtk-query-immodules-2.0 > etc/gtk-2.0/gtk.immodules
