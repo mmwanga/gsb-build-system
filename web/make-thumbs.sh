6c6cb7f4a1e0c94223138d5602db52acde5d2bf5
Chip Cuccio <chipster@gnomeslackbuild.org>
2005-08-18 16:50:32 +0000

    Modified/tweaked screenshot funcs./page

--- web/make-thumbs.sh
+++ web/make-thumbs.sh
@@ -2,6 +2,8 @@
 
 # $Id$
 
+HTML_FILE="../common/screenshots_inc.html"
+
 clear
 
 echo "removing existing thumbs ..."
@@ -12,22 +14,27 @@ for i in $( ls thumb-* ); do
     rm -f $i
     echo "      deleted '$i'"
 done
-sleep 1
 
 echo ""
 echo "generating thumbnails from regular images (this may take a minute) ..."
 echo ""
 
 cd ../
+echo "" > $HTML_FILE
 for i in $( ls *.png ); do
-    echo "      Generating 'thumb-$i'"
+    PAGE_ENTRY="  <a href=\"/screenies/$i\"><img src=
+   \"/screenies/thumbs/thumb-$i\" alt=\"$i\"
+    class=\"screenthumb\" title=\"$i\" /></a>
+"
+    echo "      Generating 'thumb-$i' and adding to HTML page..."
     convert -geometry 150x150 $i thumbs/thumb-$i
+    echo "$PAGE_ENTRY" >> $HTML_FILE
 done
 
 echo ""
 echo "Adding screenshots and thumbnails into SVN"
 
-svn --quiet add thumbs/*.png
+svn --quiet add thumbs/*.png *.png
 
 echo ""
 echo "DONE!"
