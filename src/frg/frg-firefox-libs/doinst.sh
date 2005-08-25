a471127c41f44b753a7dbff79a59bad9c32d55f6
Alak Trakru <freerock@gmail.com>
2005-08-25 19:45:45 +0000

    trunk:   * src: frg-firefox-libs: Fixes from Darren Austin.

--- src/frg/frg-firefox-libs/doinst.sh
+++ src/frg/frg-firefox-libs/doinst.sh
@@ -2,8 +2,8 @@ if [ -L usr/lib/firefox ]; then
     for nsslib in libnspr4.so libnss3.so libplc4.so libplds4.so libsmime3.so libsoftokn3.so libssl3.so
       do
       if [ ! -f usr/lib/$nsslib ]; then
-	  ( cd usr/lib ; rm -rf $nsslib )
-	  ( cd usr/lib ; ln -sf firefox/$nsslib $nsslib )
+( cd usr/lib ; rm -rf $nsslib )
+( cd usr/lib ; ln -sf firefox/$nsslib $nsslib )
       fi
     done
 fi
