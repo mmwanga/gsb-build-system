c10f0bcf754bd2d83059429cf960e163a891f3c0
Jason Woodward <woodwardj@jaos.org>
2005-09-12 19:13:23 +0000

    added nvu slackbuild

---
+++ src/other/nvu/doinst.sh
@@ -0,0 +1,7 @@
+NVUDIR=usr/lib64/nvu-1.0
+REGXPCOM=../../../$NVUDIR/regxpcom
+REGCHROME=../../../$NVUDIR/regchrome
+NVU=../../../$NVUDIR/nvu-bin
+
+# Register the default extensions
+( cd $NVUDIR && ./run-mozilla.sh $REGXPCOM && ./run-mozilla.sh $REGCHROME && ./run-mozilla.sh $NVU -register )
