ea33296e6a15de3b75d28e5989acbbede85ffbc6
Alak Trakru <freerock@gmail.com>
2005-10-03 03:08:21 +0000

    frg_iso_install: patch to fix frg_check_reqs.sh from Mike Reynolds

--- bin/installers/frg_iso_install/install_scripts/frg_check_reqs.sh
+++ bin/installers/frg_iso_install/install_scripts/frg_check_reqs.sh
@@ -2,8 +2,8 @@
 #
 # Script to check some pre-requisites for FRG
 #
-
-FRGROOT=`dirname $0`
+# Fixed by Mike Reynolds mike@marskreations.com
+# Sun Oct  2 23:05:17 EDT 2005
 
 REQUIRED_PACKAGES="
 perl
@@ -22,9 +22,14 @@ x11-xnest
 "
 
 check_packages() {
-    PACKAGE=$1
-
-    if [ ! -f "/var/log/packages/$PACKAGE-*" ]; then
+    PACKAGE=/var/log/packages/$i*
+    #The 2> /dev/null is necessary for packages with more than one match
+    #EX: python has;
+	#python-2.4-i486-1
+	#python-demo-2.4-noarch-1
+	#python-tools-2.4-noarch-1
+    #Script needs a bit more work if more precise pkg matching is needed
+    if [ ! -f $PACKAGE 2> /dev/null ]; then
 	MISSING_PACKAGES="$MISSING_PACKAGES $i"
     fi
 }
@@ -36,7 +41,7 @@ done
 
 if [ -n "$MISSING_PACKAGES" ]; then
     echo ""
-    echo "The following packages are missing:"
+    echo "The following package(s) are missing:"
     echo ""
 
     for i in $MISSING_PACKAGES
@@ -47,4 +52,6 @@ if [ -n "$MISSING_PACKAGES" ]; then
     echo ""
     echo "Please install them from your Slackware CDs or a slackware mirror"
     echo ""
+else
+    echo "All pre-requisites met"
 fi
