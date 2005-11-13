ef42517977d63e8bacb22b028b49681f82ef8ce2
Alak Trakru <freerock@gmail.com>
2005-11-13 16:55:41 +0000

    gnome-extra-themes: patch from Thomas Zajic.

--- src/gnome/themes/gnome-extra-themes/doinst.sh
+++ src/gnome/themes/gnome-extra-themes/doinst.sh
@@ -1,7 +1,8 @@
 for i in Suede firefox-thunderbird TiCons gcezanne Edge-Icons-0.4 \
 	etiquette DebianIcons-vitamin-0.4a Glossed MacOS-X Kreski-Lines \
-	MMX-Mercury-L Marcintesh_OS-L-Icon-Color_Pack Xi-GNOME d3a-icons \
-	gartoon exquisite gnant lila-gnome
+	MMX-Mercury-L Marcintesh_OS-L-Black-Icons Marcintesh_OS-L-Blue-Icons \
+	Marcintesh_OS-L-Green-Icons Marcintesh_OS-L-White-Icons Xi-GNOME d3a-icons \
+	gartoon exquisite gnant Lila
 do
     if [ -e usr/share/icons/$i/icon-theme.cache ]; then
         rm -f usr/share/icons/$i/icon-theme.cache
