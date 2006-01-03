a2137c5a7501dbd9a7cef55279f9c007a743106a
Alak Trakru <freerock@gmail.com>
2006-01-03 05:17:37 +0000

    put gtk-query-immodules-2.0 back into the doinst.sh

--- src/gnome/platform/gtk+2/doinst.sh
+++ src/gnome/platform/gtk+2/doinst.sh
@@ -1,2 +1,2 @@
 usr/bin/gdk-pixbuf-query-loaders > etc/gtk-2.0/gdk-pixbuf.loaders
-#no longer there usr/bin/gtk-query-immodules-2.0 > etc/gtk-2.0/gtk.immodules
+usr/bin/gtk-query-immodules-2.0 > etc/gtk-2.0/gtk.immodules
