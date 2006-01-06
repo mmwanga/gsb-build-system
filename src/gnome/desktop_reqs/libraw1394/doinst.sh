e63892d15682075fc0123ba324e8e135b2c53d9b
Alak Trakru <freerock@gmail.com>
2006-01-06 00:44:15 +0000

    bug fixes from Tadgy

--- src/gnome/desktop_reqs/libraw1394/doinst.sh
+++ src/gnome/desktop_reqs/libraw1394/doinst.sh
@@ -1,4 +1,4 @@
 if [ ! -e dev/raw1394 ]; then
   chroot . usr/bin/mknod -m 660 dev/raw1394 c 171 0
-  chroot . usr/bin/chown root.media dev/raw1394
+  chroot . usr/bin/chown root.video dev/raw1394
 fi
