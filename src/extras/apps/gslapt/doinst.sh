a78b3d593959f36234dae708fbbde5622fff670b
Alak Trakru <freerock@gmail.com>
2006-05-05 12:38:55 +0000

    gslapt: patch from Lukasz Stelmach to fix the doinst.sh file

--- src/extras/apps/gslapt/doinst.sh
+++ src/extras/apps/gslapt/doinst.sh
@@ -1,11 +1,11 @@
 DESKTOPFILE=usr/share/applications/gslapt.desktop
 if [ -f $DESKTOPFILE ]; then
   if [ -x usr/bin/gnomesu ]; then
-    sed -i.bak -re "s/(Exec=)(\/usr\/sbin\/gslapt)/\1gksudo \2/" $DESKTOPFILE
+    sed -i.bak -re "s/(Exec=)(\/usr\/sbin\/gslapt)/\1gnomesu \2/" $DESKTOPFILE
   elif [ -x usr/bin/gksu ]; then
     sed -i.bak -re "s/(Exec=)(\/usr\/sbin\/gslapt)/\1gksu \2/" $DESKTOPFILE
   elif [ -x usr/bin/gksudo ]; then
-    sed -i.bak -re "s/(Exec=)(\/usr\/sbin\/gslapt)/\1gnomesu \2/" $DESKTOPFILE
+    sed -i.bak -re "s/(Exec=)(\/usr\/sbin\/gslapt)/\1gksudo \2/" $DESKTOPFILE
   elif [ -x usr/bin/kdesu ]; then
     sed -i.bak -re "s/(Exec=)(\/usr\/sbin\/gslapt)/\1kdesu \2/" $DESKTOPFILE
   fi
