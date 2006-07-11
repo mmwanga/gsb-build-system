a5a6519fd9d6f42b357c9f6752727f271a9a4aae
Alak Trakru <freerock@gmail.com>
2006-07-11 03:09:53 +0000

    remove clearlooks from the build, its included in gtk-engines

--- src/gnome/themes/clearlooks/doinst.sh
+++
@@ -1,8 +0,0 @@
-for i in Clearlooks Clearlooks-DeepSky Clearlooks-Olive Clearlooks-Quicksilver
-do
-    if [ -e usr/share/icons/$i/icon-theme.cache ]; then
-        rm -f usr/share/icons/$i/icon-theme.cache
-    fi
-	usr/bin/gtk-update-icon-cache -q -f --ignore-theme-index usr/share/icons/$i
-done
-
