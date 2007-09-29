7dc97e490f68a4eb4c6ca45387836c6f5f259e65
Steve Kennedy <steve@gnomeslackbuild.org>
2007-09-29 16:31:22 +0000

    doinst.sh fixes for scrollkeeper database rebuild.

--- src/gnome/platform/scrollkeeper/doinst.sh
+++ src/gnome/platform/scrollkeeper/doinst.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+if [ -x usr/bin/scrollkeeper-rebuilddb ]; then
+	echo -n "Creating scrollkeeper database..."
+	usr/bin/scrollkeeper-rebuilddb -q -p var/lib/scrollkeeper
+	echo "done."
+fi;
+
 if [ -x usr/bin/scrollkeeper-update ]; then
-  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper 1> /dev/null 2> /dev/null
+	usr/bin/scrollkeeper-update -p var/lib/scrollkeeper 1> /dev/null 2> /dev/null
 fi
