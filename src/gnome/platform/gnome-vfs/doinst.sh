fecb28c95c04bfb286a32edd4f4231857679aaa3
Steve Kennedy <steve@gnomeslackbuild.org>
2007-09-29 15:29:37 +0000

    doinst.sh tweaks for proper chroot.

--- src/gnome/platform/gnome-vfs/doinst.sh
+++ src/gnome/platform/gnome-vfs/doinst.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-if [ -x /usr/bin/update-mime-database ]; then
-	/usr/bin/update-mime-database usr/share/mime/ &> /dev/null
+if [ -x usr/bin/update-mime-database ]; then
+	usr/bin/update-mime-database usr/share/mime/ &> /dev/null
 fi
