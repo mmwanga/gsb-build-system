c9ad9522ba8bc0bd4ce7bc6278affbc0b56d87ff
Steve Kennedy <steve@gnomeslackbuild.org>
2007-09-30 18:46:07 +0000

    Split off from desktop some applications into a new gnome/applications directory.

--- src/gnome/desktop/gnome-panel/doinst.sh
+++ src/gnome/desktop/gnome-panel/doinst.sh
@@ -1,10 +1,3 @@
-GCONF_SOURCE=`chroot . usr/bin/gconftool-2 --get-default-source | sed 's#/etc#etc#'`
-for i in etc/gconf/schemas/*.entries
-do
-	chroot . usr/bin/gconftool-2 --config-source=$GCONF_SOURCE --load $i
-	chroot . usr/bin/gconftool-2 --config-source=$GCONF_SOURCE --load $i /apps/panel
-done
-
 if [ -x usr/bin/scrollkeeper-update ]; then
   usr/bin/scrollkeeper-update -p var/lib/scrollkeeper 1> /dev/null 2> /dev/null
 fi
