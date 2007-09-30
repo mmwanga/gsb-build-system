25d36cef31ce1a300f493aa1ea258aaeec42950b
Steve Kennedy <steve@gnomeslackbuild.org>
2007-09-30 19:27:37 +0000

    Gstreamer updated.

--- src/gnome/desktop/gstreamer/doinst.sh
+++ src/gnome/desktop/gstreamer/doinst.sh
@@ -1,7 +1,3 @@
-if [ -x usr/bin/scrollkeeper-update ]; then
-  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper 1> /dev/null 2> /dev/null
-fi
-
 if [ -x usr/bin/update-mime-database ]; then
   usr/bin/update-mime-database usr/share/mime 1> /dev/null 2> /dev/null
 fi
@@ -10,22 +6,6 @@ if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
-config() {
-  NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-#config
-
-chroot . sbin/ldconfig 1> /dev/null 2> /dev/null
-
 if [ -x usr/bin/gst-register ]; then
-  chroot . usr/bin/gst-register 1> /dev/null 2> /dev/null
+  usr/bin/gst-register 1> /dev/null 2> /dev/null
 fi
