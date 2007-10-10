fa731c7cc709d5a218603092a37419c66d0afafa
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-10 16:47:07 +0000

    Separate into applications.

--- src/gnome/desktop/alacarte/doinst.sh
+++
@@ -1,6 +0,0 @@
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
-fi
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
-fi
