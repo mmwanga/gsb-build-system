3bf49b53d4d6e2036fe49548523777fe95e072a2
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-11 22:46:27 +0000

    gconf fixes

--- src/desktop/gnome-control-center/doinst.sh
+++ src/desktop/gnome-control-center/doinst.sh
@@ -1,3 +1,26 @@
+ldconfig -r .
+
+function install_file() {
+  # $1 = File to process
+
+  FILE="$(dirname "$1")/$(basename "$1" .new)"
+  if [ ! -e "$FILE" ]
+  then
+    mv "$FILE.new" "$FILE"
+  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
+  then
+    #     |--------|--------------------------------------------------|
+    echo "WARNING: $FILE has been customised."
+    echo "         Examine the $FILE.new file"
+    echo "         and integrate any changes into the custom file."
+    echo
+  else
+    rm -f "$FILE.new"
+  fi
+}
+
+install_file etc/xdg/menus/gnomecc.menu
+
 if [ -x usr/bin/scrollkeeper-update ]; then
   usr/bin/scrollkeeper-update -p var/lib/scrollkeeper 1> /dev/null 2> /dev/null
 fi
--- src/desktop/gnome-menus/doinst.sh
+++ src/desktop/gnome-menus/doinst.sh
@@ -1,20 +1,32 @@
-config() {
-  NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
-    rm $NEW
+# Version: 0.1.1
+
+ldconfig -r .
+
+function install_file() {
+  # $1 = File to process
+
+  FILE="$(dirname "$1")/$(basename "$1" .new)"
+  if [ ! -e "$FILE" ]
+  then
+    mv "$FILE.new" "$FILE"
+  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
+  then
+    #     |--------|--------------------------------------------------|
+    echo "WARNING: $FILE has been customised."
+    echo "         Examine the $FILE.new file"
+    echo "         and integrate any changes into the custom file."
+    echo
+  else
+    rm -f "$FILE.new"
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 for i in applications preferences settings
-do    
-	config etc/xdg/menus/$i.menu.new
+do
+    install_file etc/xdg/menus/$i.menu.new
 done
 
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
+
