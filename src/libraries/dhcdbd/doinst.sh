af0ad6ed623e02fb3b649a45d2b8c001f41f5254
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-14 17:35:16 +0000

    Added dhcdbd.  NetworkManager fixes.

---
+++ src/libraries/dhcdbd/doinst.sh
@@ -0,0 +1,39 @@
+# Version: 0.1.1
+
+ldconfig -r .
+
+function install_file() {
+  # $1 = File to process
+
+  FILE="$(dirname "$1")/$(basename "$1" .new)"
+  if [ ! -e "$FILE" ]
+  then
+    mv "$FILE.new" "$FILE"
+  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
+  then
+    #     |--------|--------------------------------------------------|
+    echo "WARNING: $FILE has been customised."
+    echo "         Examine the $FILE.new file"
+    echo "         and integrate any changes into the custom file."
+    echo
+  else
+    rm -f "$FILE.new"
+  fi
+}
+
+install_file etc/dbus-1/system.d/dhcdbd.conf
+
+##  
+## Make dbus executable if dhcdbd is installed
+##
+if [ ! -x etc/rc.d/rc.messagebus ]; then
+    chmod +x etc/rc.d/rc.messagebus;
+fi;
+
+##
+## Restart dbus (reload dhcdbd info), and start services
+##
+if [ -x etc/rc.d/rc.messagebus ]; then
+    . etc/rc.d/rc.messagebus restart;
+fi;
+
