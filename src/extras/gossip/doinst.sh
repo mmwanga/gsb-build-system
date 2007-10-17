eec3248c6e4e7dbe3035b52592ff254369400b8e
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-17 22:20:19 +0000

    New gsb.extras and more cleanup.

--- src/extras/gossip/doinst.sh
+++
@@ -1,8 +0,0 @@
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
-fi
-
-if [ -x usr/bin/scrollkeeper-update ]; then
-  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper 1> /dev/null 2> /dev/null
-fi
-
