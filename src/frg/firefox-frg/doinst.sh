61d3a6c7566b13e100ded8ad879127000d8aada8
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-17 23:12:57 +0000

    Move frg-mozilla, etc into deprecated.

--- src/frg/firefox-frg/doinst.sh
+++
@@ -1,8 +0,0 @@
-
-FIREFOXDIR=usr/lib/firefox-1.0.6
-REGXPCOM=../../../$FIREFOXDIR/regxpcom
-REGCHROME=../../../$FIREFOXDIR/regchrome
-FIREFOX=../../../$FIREFOXDIR/firefox-bin
-
-# Register the default extensions
-( cd $FIREFOXDIR && ./run-mozilla.sh $REGXPCOM && ./run-mozilla.sh $REGCHROME && ./run-mozilla.sh $FIREFOX -register )
