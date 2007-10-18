00d8670fe0c20cefde2c1f3f379fd7a3ed7ac775
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-18 16:23:56 +0000

    rarian-sk-rebuild / rarian-sk-update

--- src/platform/rarian/doinst.sh
+++ src/platform/rarian/doinst.sh
@@ -1,9 +1,9 @@
-if [ -x usr/bin/rarian-rebuilddb ]; then
+if [ -x usr/bin/rarian-sk-rebuild ]; then
 	echo -n "Creating rarian database..."
-	usr/bin/rarian-rebuilddb -q -p var/lib/rarian
+	usr/bin/rarian-sk-rebuild -q -p var/lib/rarian
 	echo "done."
 fi;
 
-if [ -x usr/bin/rarian-update ]; then
-	usr/bin/rarian-update -p var/lib/rarian 1> /dev/null 2> /dev/null
+if [ -x usr/bin/rarian-sk-update ]; then
+	usr/bin/rarian-sk-update -p var/lib/rarian 1> /dev/null 2> /dev/null
 fi
