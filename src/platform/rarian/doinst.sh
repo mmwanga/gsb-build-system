927712308949d66c1619fc659be071ec3a4fc89d
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-18 16:49:58 +0000

    Switch to rarian style index.

--- src/platform/rarian/doinst.sh
+++ src/platform/rarian/doinst.sh
@@ -1,9 +1,5 @@
 if [ -x usr/bin/rarian-sk-rebuild ]; then
-	echo -n "Creating rarian database..."
-	usr/bin/rarian-sk-rebuild -q -p var/lib/rarian
+	echo -n "Updating rarian database..."
+	usr/bin/rarian-sk-update -p var/lib/rarian --clean-index
 	echo "done."
 fi;
-
-if [ -x usr/bin/rarian-sk-update ]; then
-	usr/bin/rarian-sk-update -p var/lib/rarian 1> /dev/null 2> /dev/null
-fi
