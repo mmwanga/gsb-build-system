686ed59059dc1e7f0f341aed79c0ad8a7d1aa0cd
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-19 13:38:10 +0000

    Typo.  Loaders > modules.

--- src/platform/pango/doinst.sh
+++ src/platform/pango/doinst.sh
@@ -1,4 +1,4 @@
 ## Create modules list in etc/pango
-if [ -x usr/bin/pango-queryloaders ]; then
-   usr/bin/pango-queryloaders > etc/pango/pango.modules;
+if [ -x usr/bin/pango-querymodules ]; then
+   usr/bin/pango-querymodules > etc/pango/pango.modules;
 fi;
