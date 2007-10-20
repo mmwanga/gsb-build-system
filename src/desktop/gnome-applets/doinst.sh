1c92fb4255afa0b327285000e588631822ceec9b
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-20 16:36:09 +0000

    Add update-desktop to doinst.sh

--- src/desktop/gnome-applets/doinst.sh
+++ src/desktop/gnome-applets/doinst.sh
@@ -1,3 +1,7 @@
 if [ -x usr/bin/rarian-sk-update ]; then
   usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
 fi
+
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
+fi
