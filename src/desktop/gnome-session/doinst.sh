be92379a6a0a9920e260ae19741c1b497c3fd2c6
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-20 23:20:04 +0000

    Preserve etc/X11/xinit/xinitrc.gnome.

--- src/desktop/gnome-session/doinst.sh
+++ src/desktop/gnome-session/doinst.sh
@@ -1,3 +1,24 @@
+function install_file() {
+  # $1 = File to process
+
+  FILE="$(dirname "$1")/$(basename "$1" .new)"
+  if [ ! -e "$FILE" ]
+  then
+    mv "$FILE.new" "$FILE"
+  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
+  then
+    #     |--------|--------------------------------------------------|
+    echo "WARNING: $FILE has been customised."
+    echo "         Examine the $FILE.new file"
+    echo "         and integrate any changes into the custom file."
+    echo
+  else
+    rm -f "$FILE.new"
+  fi
+}
+
+install_file etc/X11/xinit/xinitrc.gnome.new
+
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
