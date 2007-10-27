f2f9180e636675fa0ebf6be49eb3486077886d0b
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-27 10:31:57 +0000

    Various updates. See ChangeLog.

--- src/applications/sabayon/doinst.sh
+++ src/applications/sabayon/doinst.sh
@@ -1,29 +1,3 @@
-# Version: 0.1.1
-
-ldconfig -r .
-
-function install_file() {
-  # $1 = File to process
-
-  FILE="$(dirname "$1")/$(basename "$1" .new)"
-  if [ ! -e "$FILE" ]
-  then
-    mv "$FILE.new" "$FILE"
-  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
-  then
-    #     |--------|--------------------------------------------------|
-    echo "WARNING: $FILE has been customised."
-    echo "         Examine the $FILE.new file"
-    echo "         and integrate any changes into the custom file."
-    echo
-  else
-    rm -f "$FILE.new"
-  fi
-}
-
-install_file etc/gconf/2/local-mandatory.path.new
-install_file etc/gconf/2/local-defaults.path.new
-
 # If the sabayon user/group don't exist, add them:
 if grep "^sabayon:x:" etc/passwd 1> /dev/null 2> /dev/null ; then
   true
@@ -40,8 +14,6 @@ if grep "^sabayon:" etc/shadow 1> /dev/null 2> /dev/null ; then
 else
   echo "sabayon:*:9797:0:::::" >> etc/shadow
 fi
-
-chroot . bin/chown -R sabayon.sabayon var/sabayon
 
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
