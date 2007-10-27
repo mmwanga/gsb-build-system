331e398dbb0c1c0f23a570d1ea6be3b9a3ad85da
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-27 18:41:53 +0000

    Various fixes and updated for accessibility.

--- src/accessibility/gnopernicus/doinst.sh
+++ src/accessibility/gnopernicus/doinst.sh
@@ -1,10 +1,10 @@
 # Update the X font indexes:
-if [ -x usr/X11R6/bin/fc-cache ]; then
-  chroot . usr/X11R6/bin/fc-cache -f
+if [ -x usr/bin/fc-cache ]; then
+  chroot . usr/bin/fc-cache -f
 fi
 
-if [ -x usr/bin/scrollkeeper-update ]; then
-  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper 1> /dev/null 2> /dev/null
+if [ -x usr/bin/rarian-sk-update ]; then
+  usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
 fi
 
 if [ -x usr/bin/update-desktop-database ]; then
