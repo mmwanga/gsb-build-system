29f8491dd70730352d0f6f2df8b9f16f1c5c1e53
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-28 15:19:29 +0000

    Fix icon problem.

--- src/extras/brasero/doinst.sh
+++ src/extras/brasero/doinst.sh
@@ -1,5 +1,11 @@
-
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
+for i in gnome hicolor
+do
+    if [ -e usr/share/icons/$i/icon-theme.cache ]; then
+        rm -f usr/share/icons/$i/icon-theme.cache
+    fi
+    usr/bin/gtk-update-icon-cache -f -q --ignore-theme-index usr/share/icons/$i
+done
