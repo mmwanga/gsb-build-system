d0683d349c8d4774fa42b328e36a8d850ef1e860
Steve Kennedy <steve@gnomeslackbuild.org>
2007-10-28 14:57:01 +0000

    Fix improper icon install.  Add slack-conflicts for gnome-control-center.

--- src/desktop/gnome-doc-utils/doinst.sh
+++ src/desktop/gnome-doc-utils/doinst.sh
@@ -2,3 +2,10 @@ if [ -x usr/bin/rarian-sk-update ]; then
   usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
 fi
 
+for i in gnome hicolor
+do
+    if [ -e usr/share/icons/$i/icon-theme.cache ]; then
+        rm -f usr/share/icons/$i/icon-theme.cache
+    fi
+    usr/bin/gtk-update-icon-cache -f -q --ignore-theme-index usr/share/icons/$i
+done
