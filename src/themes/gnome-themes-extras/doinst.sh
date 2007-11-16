a62bc5a761768660c2639373a89a379aebe9a65f
Steve Kennedy <steve@gnomeslackbuild.org>
2007-11-16 00:30:31 +0000

    themes cleanup

--- src/themes/gnome-themes-extras/doinst.sh
+++
@@ -1,8 +0,0 @@
-for i in Amaranth Gorilla Nuvola Lush Wasp
-do
-    if [ -e usr/share/icons/$i/icon-theme.cache ]; then
-        rm -f usr/share/icons/$i/icon-theme.cache
-    fi
-	usr/bin/gtk-update-icon-cache -q -f --ignore-theme-index usr/share/icons/$i
-done
-
