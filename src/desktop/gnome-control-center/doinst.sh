312c3780bc3e403cc428a12d667a4f784bff1309
Steve Kennedy <steve@gnomeslackbuild.org>
2007-11-16 15:43:33 +0000

    Keep gconf quiet

--- src/desktop/gnome-control-center/doinst.sh
+++ src/desktop/gnome-control-center/doinst.sh
@@ -33,5 +33,5 @@ fi
 
 ## Allow users to modify the list of user-specified or Favorite applications
 if [ -x usr/bin/gconftool-2 ]; then
-    usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type string --set /desktop/gnome/applications/main-menu/lock-down/user_modifiable_apps True
+    usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type string --set /desktop/gnome/applications/main-menu/lock-down/user_modifiable_apps True >/dev/null 2>&1
 fi;
