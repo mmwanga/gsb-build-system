c71e3b9198de68aae50032fa061faa752da035cc
Steve Kennedy <steve@gnomeslackbuild.org>
2007-11-16 15:38:53 +0000

    Add gconf settings for user apps

--- src/desktop/gnome-control-center/doinst.sh
+++ src/desktop/gnome-control-center/doinst.sh
@@ -30,3 +30,8 @@ fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
+
+## Allow users to modify the list of user-specified or Favorite applications
+if [ -x usr/bin/gconftool-2 ]; then
+    usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type string --set /desktop/gnome/applications/main-menu/lock-down/user_modifiable_apps True
+fi;
