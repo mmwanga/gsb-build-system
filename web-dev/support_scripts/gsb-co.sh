4d85cf1c54f3804eb7b26a6be54f37f353d8077b
Chip Cuccio <chipster@gnomeslackbuild.org>
2007-11-24 20:52:05 +0000

    re-do

--- web-dev/support_scripts/gsb-co.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-svn up web-dev/
-svn co http://svn.gnomeslackbuild.org/gsb/trunk/web-dev
--- web-dev/support_scripts/migrate-to-prod.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# syncs staging site to prod.
-
-rsync -trPv --delete --exclude=.svn ./web-dev \
-    $HOME/webs/gnomeslackbuild.org/
