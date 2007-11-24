bf10f1a9717aa797710ae60418dd733e4944706f
Steve Kennedy <steve@gnomeslackbuild.org>
2007-11-24 19:45:12 +0000

    Add gdesklets. Path runtime fix for deluge. Add swig for gnucash svn building. Update glib2 version.

--- src/extras/gDesklets/doinst.sh
+++ src/extras/gDesklets/doinst.sh
@@ -5,3 +5,8 @@ fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
+
+if [ -e usr/share/icons/gnome/icon-theme.cache ]; then
+        rm -f usr/share/icons/gnome/icon-theme.cache
+fi
+usr/bin/gtk-update-icon-cache -f -q --ignore-theme-index usr/share/icons/gnome
