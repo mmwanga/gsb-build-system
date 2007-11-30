ed7a455ecd662bfa75fa7e24d6f29e2789e0f89b
Steve Kennedy <steve@gnomeslackbuild.org>
2007-11-30 21:34:38 +0000

    Many updates.  See ChangeLog.

--- src/compiz/compiz/doinst.sh
+++ src/compiz/compiz/doinst.sh
@@ -2,4 +2,4 @@ if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
-echo "(Please wait. Installing schemas.)"
+echo "(Please wait, installing schemas.  This may take a while.)"
