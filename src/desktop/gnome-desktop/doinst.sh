8680998f9477b12cc11576e8b759f99145ed7c4f
Steve Kennedy <steve@gnomeslackbuild.org>
2007-12-07 18:02:49 +0000

    Removed NetworkManager/dhcdbd (moved to lib/gsb/deprecated).  Use NetworkManager in testing. Added a bunch of Mono bindings for Banshee. Added Banshee. Cleaned up pkgconfig files on X86_64 platforms. Added chipster's Background/Splash screen. Forced Tango to be default icon theme. Forced GSB Background. Forced GSB Splash.
    Note: Banshee isn't quite working yet.  Needs sqlite bindings.
    But I'm off to get drunk, so I'll finish that later.  :^)

--- src/desktop/gnome-desktop/doinst.sh
+++ src/desktop/gnome-desktop/doinst.sh
@@ -5,3 +5,6 @@ fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
+
+# Set GSB background as default
+usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type string --set /desktop/gnome/background/picture_filename backgrounds/gsb-background.png
--- src/desktop/gnome-session/doinst.sh
+++ src/desktop/gnome-session/doinst.sh
@@ -22,3 +22,6 @@ install_file etc/X11/xinit/xinitrc.gnome.new
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
+
+# Set GSB splash screen as default
+usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type string --set /apps/gnome-session/options/splash_image splash/gsb-splash.png
