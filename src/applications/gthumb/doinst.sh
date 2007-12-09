5a3ed1e91589fae429acfcbe0234c06c992beea1
Steve Kennedy <steve@gnomeslackbuild.org>
2007-12-09 22:50:25 +0000

    Add gthumb as autophoto command when digital cameras plugged in.

--- src/applications/gthumb/doinst.sh
+++ src/applications/gthumb/doinst.sh
@@ -5,3 +5,14 @@ fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
+if [ -x usr/bin/rarian-sk-update ]; then
+  usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
+fi
+
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
+fi
+
+# Set gthumb as auto camera command
+usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type string --set /desktop/gnome/volume_manager/autophoto_command "gthumb --import-photos" 2> /dev/null 1> /dev/null
+usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type boolean --set /desktop/gnome/volume_manager/autophoto True
