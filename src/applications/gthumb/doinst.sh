735acc5c7a8962fce019ee4cbb4f515d0d0cb31b
Steve Kennedy <steve@gnomeslackbuild.org>
2008-01-11 13:23:16 +0000

    Add icon refresh for gthumb

--- src/applications/gthumb/doinst.sh
+++ src/applications/gthumb/doinst.sh
@@ -5,13 +5,15 @@ fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
+
 if [ -x usr/bin/rarian-sk-update ]; then
   usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
 fi
 
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+	rm -f usr/share/icons/hicolor/icon-theme.cache
 fi
+usr/bin/gtk-update-icon-cache -f -q usr/share/icons/hicolor
 
 # Set gthumb as auto camera command
 usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type string --set /desktop/gnome/volume_manager/autophoto_command "gthumb --import-photos" 2> /dev/null 1> /dev/null
