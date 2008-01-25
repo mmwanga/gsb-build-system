df79b274da555833f43cf488800f5a266fea0e1d
Steve Kennedy <steve@gnomeslackbuild.org>
2008-01-25 16:02:52 +0000

    Bump to deluge 0.5.8.1

--- lib/perl/GSB/Extras.pm
+++ lib/perl/GSB/Extras.pm
@@ -36,8 +36,8 @@ our %extras =
 			     'src' => 'tar.bz2',
 			    },
    'deluge'              => {
-                 'ver' => '0.5.8',
-                 'url' => 'http://download.deluge-torrent.org/tarball/0.5.8',
+                 'ver' => '0.5.8.1',
+                 'url' => 'http://download.deluge-torrent.org/tarball/0.5.8.1',
                  'src' => 'tar.gz',
                             },
    'devede'              => {
