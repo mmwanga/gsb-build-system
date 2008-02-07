209e348332c88bfc1bc0bb44c22dca91cc5421a1
Steve Kennedy <steve@gnomeslackbuild.org>
2008-02-07 23:18:46 +0000

    Fix up compiz.  Add new xinitrc script and GDM session script. Add a gconf tweak to remove compiz shadows from GNOME dock and menus (which caused a weird white bar around stuff)

--- src/compiz/compiz/doinst.sh
+++ src/compiz/compiz/doinst.sh
@@ -1,5 +1,27 @@
+function install_file() {
+  # $1 = File to process
+
+  FILE="$(dirname "$1")/$(basename "$1" .new)"
+  if [ ! -e "$FILE" ]
+  then
+    mv "$FILE.new" "$FILE"
+  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
+  then
+    #     |--------|--------------------------------------------------|
+    echo "WARNING: $FILE has been customised."
+    echo "         Examine the $FILE.new file"
+    echo "         and integrate any changes into the custom file."
+    echo
+  else
+    rm -f "$FILE.new"
+  fi
+}
+
+install_file etc/X11/xinit/xinitrc.compiz-gnome.new
+
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
-echo "(Please wait, installing schemas.  This may take a while.)"
+# Fix for weird white bar around menus and dock
+usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type string --set /apps/compiz/plugins/decoration/allscreens/options/shadow_match "2 !dock"1> /dev/null 2> /dev/null
