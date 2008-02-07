72ff4b0b879662138c4899f9a36800014e93150c
Steve Kennedy <steve@gnomeslackbuild.org>
2008-02-07 22:29:45 +0000

    Fix doinst.sh error in gnome-themes (didn't refresh new icons)

--- src/desktop/gnome-themes/doinst.sh
+++ src/desktop/gnome-themes/doinst.sh
@@ -1,10 +1,9 @@
 for i in Crux HighContrast HighContrast-SVG HighContrastInverse \
           HighContrastLargePrintInverse HighContrastLargePrint \
-	LargePrint LowContrast LowContrastLargePrint Mist gnome hicolor \
+	LargePrint LowContrast LowContrastLargePrint Mist gnome hicolor
 do
     if [ -e usr/share/icons/$i/icon-theme.cache ]; then
         rm -f usr/share/icons/$i/icon-theme.cache
     fi
 	usr/bin/gtk-update-icon-cache -q -f --ignore-theme-index usr/share/icons/$i
 done
-
