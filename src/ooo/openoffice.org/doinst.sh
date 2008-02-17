12996d7f9a2d700889ca0bfa3a3fbc505d39b00f
Steve Kennedy <steve@gnomeslackbuild.org>
2008-02-17 22:06:24 +0000

    Update splash Add icon refresh to doinst Update oo.org slackbuild

--- src/ooo/openoffice.org/doinst.sh
+++ src/ooo/openoffice.org/doinst.sh
@@ -5,3 +5,8 @@ fi
 if [ -x usr/bin/update-mime-database ]; then
   usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+        rm -f usr/share/icons/hicolor/icon-theme.cache
+fi
+usr/bin/gtk-update-icon-cache -f -q usr/share/icons/hicolor
