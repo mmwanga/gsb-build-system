7336e61b67fe9c9d6a9f2f1d760c989a31dca33a
Steve Kennedy <steve@gnomeslackbuild.org>
2008-02-18 21:56:56 +0000

    Add package gnome-ooo-menus Add package gnome-kde-menus Add package gnome-xfce-menus

---
+++ src/desktop/gnome-xfce-menus/doinst.sh
@@ -0,0 +1,8 @@
+# Icon cache refresh
+for i in gnome hicolor
+do
+        if [ -e usr/share/icons/$i/icon-theme.cache ]; then
+                rm -f usr/share/icons/$i/icon-theme.cache
+        fi
+        usr/bin/gtk-update-icon-cache -f -q usr/share/icons/$i
+done
