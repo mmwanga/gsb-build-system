4172384be3b1bf7f44498ab783cff6a215b3f7e2
Steve Kennedy <steve@gnomeslackbuild.org>
2008-03-16 13:35:43 +0000

    Move gnome-menus-ooo to ooo section

--- src/desktop/gnome-menus-ooo/doinst.sh
+++
@@ -1,8 +0,0 @@
-# Icon cache refresh
-for i in gnome hicolor
-do
-        if [ -e usr/share/icons/$i/icon-theme.cache ]; then
-                rm -f usr/share/icons/$i/icon-theme.cache
-        fi
-        usr/bin/gtk-update-icon-cache -f -q usr/share/icons/$i
-done
