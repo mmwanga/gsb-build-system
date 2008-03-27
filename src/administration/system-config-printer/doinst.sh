4e33abdf047dbae68d6eb001112379b704687e47
Steve Kennedy <steve@gnomeslackbuild.org>
2008-03-27 20:51:14 +0000

    * Update svn:ignore properties on a few admin directories * Update all SlackBuilds to have --mandir=/usr/man * Update admin gconf packages for gconf restart in doinst.sh

--- src/administration/system-config-printer/doinst.sh
+++ src/administration/system-config-printer/doinst.sh
@@ -1,24 +1,5 @@
 ldconfig -r .
 
-function install_file() {
-  # $1 = File to process
-
-  FILE="$(dirname "$1")/$(basename "$1" .new)"
-  if [ ! -e "$FILE" ]
-  then
-    mv "$FILE.new" "$FILE"
-  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
-  then
-    #     |--------|--------------------------------------------------|
-    echo "WARNING: $FILE has been customised."
-    echo "         Examine the $FILE.new file"
-    echo "         and integrate any changes into the custom file."
-    echo
-  else
-    rm -f "$FILE.new"
-  fi
-}
-
 install_file etc/dbus-1/system.d/newprinternotification.conf.new
 
 if [ -x usr/bin/update-desktop-database ]; then
