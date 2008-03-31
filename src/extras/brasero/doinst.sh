c5de6bea87341506c9dc2c808e9d940d7d4f3024
Steve Kennedy <steve@gnomeslackbuild.org>
2008-03-31 16:14:33 +0000

    * Fix brasero mime settings * Upgrade brasero and deluge

--- src/extras/brasero/doinst.sh
+++ src/extras/brasero/doinst.sh
@@ -10,7 +10,7 @@ if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
-for i in gnome hicolor
+for i in gnome hicolor HighContrastLargePrint HighContrastLargePrintInverse LowContrastLargePrint;
 do
     if [ -e usr/share/icons/$i/icon-theme.cache ]; then
         rm -f usr/share/icons/$i/icon-theme.cache
