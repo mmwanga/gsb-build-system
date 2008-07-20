8f9026b7234ea4cf2f71b32656afdc88bf6e550b
Darren Austin <darren@afterdark.org.uk>
2008-07-20 02:12:39 +0000

    Do not create a sabayon group.  It's completely un-necessary. If a sabayon group already exists, remove it. If the sabayon user exists, and is in the sabayon group, change it's group to GID 100 (which should be the users group on Slackware).

--- src/administration/sabayon/doinst.sh
+++ src/administration/sabayon/doinst.sh
@@ -1,12 +1,25 @@
-# If the sabayon user/group don't exist, add them:
-if ! grep "^sabayon:" etc/group 1>/dev/null 2>&1; then
-  echo "sabayon:x:63:" >>etc/group
+# Remove the sabayon group if it exists.
+# This is needed because a previous install may have added the sabayon group
+# which isn't actually required if the sabayon user is made a member of
+# group users.  It's just cruft to add this when not required.
+if grep "^sabayon:" etc/group >/dev/null 2>&1; then
+  cat etc/group >etc/group.gsb
+  cat etc/group.gsb | grep -v "^sabayon:" >etc/group
+  if [ $? -ne 0 ]; then
+    # Don't leave etc/group in an unknown state.
+    cat etc/group.gsb >etc/group
+    #     |--------|--------------------------------------------------|
+    echo "WARNING: Failed to remove old sabayon group."
+  fi
+  rm etc/group.old
+
+  # Make sure the sabayon user doesn't have an invalid group id.
+  chroot . /usr/sbin/usermod -g 100 sabayon
 fi
-if ! grep "^sabayon:" etc/gshadow 1>/dev/null 2>&1; then
-  echo "sabayon:*::" >>etc/gshadow
-fi
+
+# If the sabayon user doesn't exist, add it.
 if ! grep "^sabayon:" etc/passwd 1>/dev/null 2>&1; then
-  echo "sabayon:x:63:63:sabayon:/var/sabayon:/bin/false" >>etc/passwd
+  echo "sabayon:x:63:100:sabayon:/var/sabayon:/bin/false" >>etc/passwd
 fi
 if ! grep "^sabayon:" etc/shadow 1>/dev/null 2>&1; then
   echo "sabayon:*:9797:0:::::" >>etc/shadow
