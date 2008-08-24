97b7ca40d8a1f6b7175bc0aeaada416ad9129f78
Steve Kennedy <steve@gnomeslackbuild.org>
2008-08-24 18:53:16 +0000

    * Add new gnome time-tracker applet 'hamster' * Move sensors-applet to applications * Move gdesklets to applications from 'extras' * Move ekiga to networking * MOve evince to office * move gnome-backgrounds to desktop * update buildlist

--- src/applications/hamster-applet/doinst.sh
+++ src/applications/hamster-applet/doinst.sh
@@ -8,10 +8,10 @@ fi
 
 for i in gnome hicolor
 do
-    if [ -e usr/share/icons/$i/icon-theme.cache ]; then
-        rm -f usr/share/icons/$i/icon-theme.cache
-    fi
-    usr/bin/gtk-update-icon-cache -f -q --ignore-theme-index usr/share/icons/$i
+        if [ -e usr/share/icons/$i/icon-theme.cache ]; then
+                rm -f usr/share/icons/$i/icon-theme.cache
+        fi
+        usr/bin/gtk-update-icon-cache -f -q usr/share/icons/$i
 done
 
 # Restart gconfd-2 if running to reload new gconf settings
