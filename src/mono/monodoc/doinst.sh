7a627b990495f908509e624fe326d2833c161dca
Steve Kennedy <steve@gnomeslackbuild.org>
2008-08-24 16:19:06 +0000

    * Reorganise some mono applications into relevant sections.

--- src/mono/monodoc/doinst.sh
+++
@@ -1,9 +0,0 @@
-# Version: 1.0 - Do not remove this line!
-
-# Update new libraries
-ldconfig -r .
-
-# Generate mono documentation index
-if [ -x usr/bin/monodoc ]; then
-	  chroot . usr/bin/monodoc --make-index 1> /dev/null 2> /dev/null
-fi;
