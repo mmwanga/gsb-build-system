f5250a8834f5e70a3eab1b92187619ef8680e0b6
Steve Kennedy <steve@gnomeslackbuild.org>
2008-08-30 15:58:53 +0000

    *** NOTE *** GSB trunk should be considered "unstable/development" for GNOME 2.24.  Most gnome packages will be from the 2.23 series.  We will use these packages in order to prepare for the stable 2.24 release. ************
    ** Add alsa-lib package to upgrade to 1.0.17a.  This is required by the latest
       pulseaudio package, libcanberra, gnome-control-panel, etc.  
       
       -> This package will replace the standard Slackware package.  However, in 1.0.17a
       there are lots of major bug fixes which should obviate the fact that a standard
       Slackware package is replaced.  
    
    * All GNOME packages upgrade to the development 2.23 series.  
    
    * PolicyKit added (policykit and policykit-gnome packages).  Full GNOME integration
      with PolicyKit now available (ie, pulseaudio, gdm, GConf, gnome-session, panel, applets,
      gnome-power-manager, etc)
      -> This seems to be the way of the future.  PolicyKit built with shadow support, since we
      at GSB try our best to avoid PAM.  :^)
    
    * Pulseaudio upgraded to latest 0.9.11 awesome version.  :^)

---
+++ src/platform/policykit/doinst.sh
@@ -0,0 +1,41 @@
+# Version: 1.0 - Do not remove this line!
+
+# Update new libraries
+ldconfig -r .
+
+# Preserve new configuration files
+function install_file() {
+  # $1 = File to process
+  FILE="$(dirname "$1")/$(basename "$1" .new)"
+  if [ ! -e "$FILE" ]
+  then
+    mv "$FILE.new" "$FILE"
+  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
+  then
+    #     |--------|--------------------------------------------------|
+    echo "WARNING: $FILE has been customised."
+    echo "         Examine the $FILE.new file"
+    echo "         and integrate any changes into the custom file."
+    echo
+  else
+    rm -f "$FILE.new"
+  fi
+}
+
+install_file etc/PolicyKit/PolicyKit.conf.new
+
+# If the policykit group doesn't exist, add them
+if ! grep "^polkituser:" etc/group >/dev/null 2>&1; then
+  echo "polkituser:x:60:polkituser" >>etc/group
+fi
+if ! grep "^polkituser:" etc/gshadow >/dev/null 2>&1; then
+  echo "polkituser:*::polkituser" >>etc/gshadow
+fi
+
+# If the polkituser user doesn't exist, add it
+if ! grep "^polkituser:" etc/passwd >/dev/null 2>&1; then
+  echo "polkituser:x:60:60:polkituser:/var/run/PolicyKit-public:/bin/false" >>etc/passwd
+fi
+if ! grep "^polkituser:" etc/shadow >/dev/null 2>&1; then
+  echo "polkituser:*:9797:0:::::" >>etc/shadow
+fi
