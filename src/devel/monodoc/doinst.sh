3587b8ff5f2ac27cf54d92bc6f3c614f8976c1db
Steve Kennedy <steve@gnomeslackbuild.org>
2008-08-30 16:23:36 +0000

    *** NOTE *** GSB trunk should be considered "unstable/development" for GNOME 2.24.  Most gnome packages will be from the 2.23 series.  We will use these packages in order to prepare for the stable 2.24 release. ************
    * Upgrade GNOME packages to latest 2.23 (or corresponding devel) version
    * Added vala C+ GNOME compiled to devel section
    * Force building of search index in monodoc package
    * Build devhelp against xulrunner-1.9.0.1
    * Move autogen from office to devel section
    * Added nemiver GNOME debugger app to devel section
    * Added gnome-build from office section
    * Added gdl from office section

---
+++ src/devel/monodoc/doinst.sh
@@ -0,0 +1,11 @@
+# Version: 1.0 - Do not remove this line!
+
+# Update new libraries
+ldconfig -r .
+
+# Generate mono documentation index
+if [ -x usr/bin/monodoc ]; then
+	  echo "Generating document search index. This will take a while..."
+	  chroot . usr/bin/monodoc --make-index 1> /dev/null 2> /dev/null
+	  chroot . usr/bin/monodoc --make-search-index 1> /dev/null 2> /dev/null
+fi;
