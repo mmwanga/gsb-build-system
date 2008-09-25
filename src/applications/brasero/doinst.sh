e2e3192d628811aacacf502c0f4ef3e40371f486
Steve Kennedy <steve@gnomeslackbuild.org>
2008-09-25 20:48:11 +0000

    * Added simplejson package to allow on-the-fly google searching from deskbar-applet * Upgrade many GNOME package versions

--- src/applications/brasero/doinst.sh
+++ src/applications/brasero/doinst.sh
@@ -15,7 +15,7 @@ do
     if [ -e usr/share/icons/$i/icon-theme.cache ]; then
         rm -f usr/share/icons/$i/icon-theme.cache
     fi
-    usr/bin/gtk-update-icon-cache -f -q --ignore-theme-index usr/share/icons/$i
+    usr/bin/gtk-update-icon-cache -f -q --ignore-theme-index usr/share/icons/$i 1> /dev/null 2> /dev/null
 done
 
 # Restart gconfd-2 if running to reload new gconf settings
