051d9c2093faca85b58acce1f614cbd507a4d1c5
Steve Kennedy <steve@gnomeslackbuild.org>
2008-10-12 13:53:21 +0000

    * Toss these

--- lib/gsb/cruft/misc/gsb_release_make.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-
-# make a release tarball for GSB
-# append gsb to all BUILD variables in the SlackBuild files
-
-
--- lib/gsb/cruft/misc/makemd5.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rm CHECKSUMS.md5; find . -name '*.tgz' -exec md5sum {} >> CHECKSUMS.MD5 \;
