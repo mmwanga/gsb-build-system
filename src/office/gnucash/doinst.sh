e2d0e3de2a75a1559512fcbf3ca46d1fae3f215e
Steve Kennedy <steve@gnomeslackbuild.org>
2008-10-12 10:54:28 +0000

    * Added patches for gnome-extras to compile against xulrunner * Bump some builds * Added inkscape poppler patch

--- src/office/gnucash/doinst.sh
+++ src/office/gnucash/doinst.sh
@@ -33,6 +33,11 @@ if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+    rm -f usr/share/icons/hicolor/icon-theme.cache
+fi
+usr/bin/gtk-update-icon-cache -f -q --ignore-theme-index usr/share/icons/hicolor
+
 # Restart gconfd-2 if running to reload new gconf settings
 if ps acx | grep -q gconfd-2 ; then
         killall -HUP gconfd-2 ;
