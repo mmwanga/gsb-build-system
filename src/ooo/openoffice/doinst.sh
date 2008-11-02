1f87dd2d7f4fd718eb3bf464e46c25d8c09ff49e
Steve Kennedy <steve@gnomeslackbuild.org>
2008-11-02 01:53:28 +0000

    * Added a "2.4" version to ooo2.4 gnome .desktop files (to keep them separate from new 3.0 package)

--- src/ooo/openoffice/doinst.sh
+++ src/ooo/openoffice/doinst.sh
@@ -1,3 +1,34 @@
+# Version: 1.0 GSB doinst.sh - Do not remove this line!
+# Copyright (c) 2007, 2008:
+#   Darren 'Tadgy' Austin <darren (at) gnomeslackbuild.org>, Coventry, UK.
+#   Steve Kennedy <steve (at) gnomeslackbuild.org>, Exeter, UK.
+# Licenced under the terms of the GNU General Public Licence version 3.
+#
+
+# Update new libraries
+ldconfig -r .
+
+# Preserve new configuration files
+function install_file() {
+  # $1 = File to process
+  FILE="$(dirname "$1")/$(basename "$1" .new)"
+  if [ ! -e "$FILE" ]
+  then
+    mv "$FILE.new" "$FILE"
+  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
+  then
+    #     |--------|--------------------------------------------------|
+    echo "WARNING: $FILE has been customised."
+    echo "         Examine the $FILE.new file"
+    echo "         and integrate any changes into the custom file."
+    echo
+  else
+    rm -f "$FILE.new"
+  fi
+}
+
+install_file etc/bash_completion.d/ooffice2.4.sh.new
+
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database -q usr/share/applications
 fi
