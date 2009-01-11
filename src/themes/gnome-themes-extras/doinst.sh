35386eca0f6b724f464fc2c85e1ec1e31e048514
Steve Kennedy <steve@gnomeslackbuild.org>
2009-01-11 18:05:07 +0000

    * ooo-3.0.0.9 updated. * various minor updates for icon caches.

--- src/themes/gnome-themes-extras/doinst.sh
+++ src/themes/gnome-themes-extras/doinst.sh
@@ -3,6 +3,6 @@ do
     if [ -e usr/share/icons/$i/icon-theme.cache ]; then
         rm -f usr/share/icons/$i/icon-theme.cache
     fi
-	usr/bin/gtk-update-icon-cache -q -f usr/share/icons/$i
+    # These don't really need icon caches
+    #usr/bin/gtk-update-icon-cache -q -f usr/share/icons/$i
 done
-
