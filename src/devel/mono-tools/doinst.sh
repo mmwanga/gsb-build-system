8eaf4051ae195505ad760e05404d1de1dd6b8941
Steve Kennedy <steve@gnomeslackbuild.org>
2009-04-26 23:17:17 +0000

    * added package gettext-perl, required by anjuta 2.26 * update various devel versions

--- src/devel/mono-tools/doinst.sh
+++ src/devel/mono-tools/doinst.sh
@@ -27,5 +27,5 @@ fi
 
 if [ -x usr/bin/monodoc ]; then
   echo "Generating monodoc search index."
-  chroot . usr/bin/monodoc --make-index > /dev/null
+  chroot . usr/bin/monodoc --make-index 1> /dev/null 2> /dev/null
 fi
