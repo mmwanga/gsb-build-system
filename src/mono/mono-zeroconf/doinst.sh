47e4609dda632c82a7815d3df16fc57d7cef3f85
Steve Kennedy <steve@gnomeslackbuild.org>
2009-04-26 23:16:04 +0000

    * remove monodoc, now in mono-tools package * update mono to 2.4 * various mono packages rebuilt and updates * added patch to fix public string declarations in mono-zerconf package

--- src/mono/mono-zeroconf/doinst.sh
+++ src/mono/mono-zeroconf/doinst.sh
@@ -1,4 +1,4 @@
 if [ -x usr/bin/monodoc ]; then
   echo "Generating monodoc search index."
-  chroot . usr/bin/monodoc --make-index > /dev/null
+  chroot . usr/bin/monodoc --make-index 1> /dev/null 2> /dev/null
 fi
