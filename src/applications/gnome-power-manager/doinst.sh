b0abffbdd1c613fe66113730348dbe8a1b00c6df
Steve Kennedy <steve@gnomeslackbuild.org>
2009-05-04 23:50:56 +0000

    * patch up bluez-gnome to remove empty device list in wizard, and update obexftp 4.4 DBUS API * Rebuilt evolution against newer bluez * fix seahorse xdg patch to stop crashing daemon * patch up broken epiphany * patch up broken gdm (particularly for configure menu) * various package version updates

--- src/applications/gnome-power-manager/doinst.sh
+++ src/applications/gnome-power-manager/doinst.sh
@@ -14,6 +14,9 @@ do
     usr/bin/gtk-update-icon-cache -f -q --ignore-theme-index usr/share/icons/$i
 done
 
+# Set GSB background as default
+usr/bin/gconftool-2 --direct --config-source=`usr/bin/gconftool-2 --get-default-source` --type string --set /apps/gnome-power-manager/ui/icon_policy always 1> /dev/null 2> /dev/null
+
 # Restart gconfd-2 if running to reload new gconf settings
 if ps acx | grep -q gconfd-2 ; then
         killall -HUP gconfd-2 ;
