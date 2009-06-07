8f500e1dace6f01d6f40aca59c11ee4bc6bb607a
Steve Kennedy <steve@gnomeslackbuild.org>
2009-06-07 14:10:42 +0000

    * now subsumed in a set

--- src/platform/gnome-keyring/doinst.sh
+++
@@ -1,9 +0,0 @@
-# Version: 1.0 - Do not remove this line!
-
-# Update new libraries
-ldconfig -r .
-
-# Restart gconfd-2 if running to reload new gconf settings
-if ps acx | grep -q gconfd-2 ; then
-  killall -HUP gconfd-2 ;
-fi
--- src/platform/libbonoboui/doinst.sh
+++
@@ -1,6 +0,0 @@
-# Version: 1.0 - Do not remove this line!
-
-# Update desktop database
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database >/dev/null 2>&1
-fi
