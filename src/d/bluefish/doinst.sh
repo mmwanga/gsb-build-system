a77558b494de82e3dbc7a66c23f28d6a0535a6f6
Steve Kennedy <steve@gnomeslackbuild.org>
2009-06-11 14:39:27 +0000

    * update portions of d set

--- src/d/bluefish/doinst.sh
+++ src/d/bluefish/doinst.sh
@@ -1,7 +1,9 @@
+# update mime database
 if [ -x usr/bin/update-mime-database ]; then
   usr/bin/update-mime-database usr/share/mime 1> /dev/null 2> /dev/null
 fi
 
+# update desktop entries
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
