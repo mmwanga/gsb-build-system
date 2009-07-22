49f3b1b4c1cb0ccbf3ac3f42b2c4e7fb12912faf
Steve Kennedy <steve@gnomeslackbuild.org>
2009-07-22 17:43:19 +0000

    * update psiconv and lib

--- src/l/heimdal-libraries/doinst.sh
+++ src/l/heimdal-libraries/doinst.sh
@@ -6,4 +6,3 @@ if [ -x usr/bin/install-info ]; then
   usr/bin/install-info usr/info/hx509.info.gz usr/info/dir >/dev/null 2>&1
   usr/bin/install-info usr/info/heimdal.info.gz usr/info/dir >/dev/null 2>&1
 fi
-
