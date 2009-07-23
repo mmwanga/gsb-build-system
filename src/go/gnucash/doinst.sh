4e85196bd724bf75bdca0f9e35e39912561a80ec
Steve Kennedy <steve@gnomeslackbuild.org>
2009-07-23 16:51:25 +0000

    * updates to go (gnome office) set

--- src/go/gnucash/doinst.sh
+++ src/go/gnucash/doinst.sh
@@ -1,38 +1,34 @@
-ldconfig ;
-
-function install_file() {
-  # $1 = File to process
-
-  FILE="$(dirname "$1")/$(basename "$1" .new)"
-  if [ ! -e "$FILE" ]
-  then
-    mv "$FILE.new" "$FILE"
-  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
-  then
-    #     |--------|--------------------------------------------------|
-    echo "WARNING: $FILE has been customised."
-    echo "         Examine the $FILE.new file"
-    echo "         and integrate any changes into the custom file."
-    echo
-  else
-    rm -f "$FILE.new"
+# Preserve new configuration files
+install_file() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
   fi
+  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 install_file etc/gnucash/config.new
 
+# update rarian database
 if [ -x usr/bin/rarian-sk-update ]; then
   usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
 fi
 
+# update mime database
 if [ -x usr/bin/update-mime-database ]; then
   usr/bin/update-mime-database usr/share/mime 1> /dev/null 2> /dev/null
 fi
 
+# update desktop entries
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
+# update hicolor icons
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     rm -f usr/share/icons/hicolor/icon-theme.cache
 fi
--- src/go/tracker/doinst.sh
+++ src/go/tracker/doinst.sh
@@ -1,38 +1,15 @@
+# update desktop entries
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
-if [ -x usr/bin/rarian-sk-update ]; then
-  usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
-fi
-
-if [ -x usr/bin/update-mime-database ]; then
-  usr/bin/update-mime-database usr/share/mime 1> /dev/null 2> /dev/null
-fi
-
-## 
-## Complain if dbus somehow was removed, and disable autostart.
-##
-if [ ! -f etc/rc.d/rc.messagebus ]; then
-    echo "Tracker requires dbus.  You appear to be missing /etc/rc.d/rc.messagebus."
-    echo "Perhaps you need to reinstall the dbus package?"
-fi;
-
-## 
-## Make dbus executable if tracker is installed
-## 
-if [ ! -x etc/rc.d/rc.messagebus ]; then
-    chmod +x etc/rc.d/rc.messagebus;
-fi;
-
-##
-## Restart dbus (reload tracker info), and start services
-## 
-#if [ -x etc/rc.d/rc.messagebus ]; then
-#   . etc/rc.d/rc.messagebus restart;
-#i;
-
+# update hicolor icons
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
-	rm -f usr/share/icons/hicolor/icon-theme.cache
+        rm -f usr/share/icons/hicolor/icon-theme.cache
 fi
-usr/bin/gtk-update-icon-cache -f -q usr/share/icons/hicolor
+usr/bin/gtk-update-icon-cache -f -q usr/share/icons/hicolor 1>/dev/null 2>/dev/null
+
+# Restart gconfd-2 if running to reload new gconf settings
+if ps acx | grep -q gconfd-2 ; then
+        killall -HUP gconfd-2 ;
+fi
