91399d95b349b564ca22691f6874baceb0c4a036
Steve Kennedy <steve@gnomeslackbuild.org>
2009-07-24 11:39:31 +0000

    * update ap set

--- src/ap/soundconverter/doinst.sh
+++ src/ap/soundconverter/doinst.sh
@@ -1,7 +1,9 @@
+# update desktop entries
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
+# update hicolor icons
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
 	rm -f usr/share/icons/hicolor/icon-theme.cache
 fi
