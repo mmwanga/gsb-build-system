28b9d9c653a953b3a73824c2d9c76ac9f01db145
Steve Kennedy <steve@gnomeslackbuild.org>
2009-07-25 19:04:37 +0000

    * update m (mono) set, clean out old packages

--- src/m/gtk-sharp/doinst.sh
+++ src/m/gtk-sharp/doinst.sh
@@ -1,4 +1,4 @@
+# update mono search index
 if [ -x usr/bin/monodoc ]; then
-  echo "Generating monodoc search index."
   chroot . usr/bin/monodoc --make-index 1> /dev/null 2> /dev/null
 fi
--- src/m/mono-tools/doinst.sh
+++ src/m/mono-tools/doinst.sh
@@ -1,31 +1,24 @@
-ldconfig -r .
-
-function install_file() {
-  # $1 = File to process
-
-  FILE="$(dirname "$1")/$(basename "$1" .new)"
-  if [ ! -e "$FILE" ]
-  then
-    mv "$FILE.new" "$FILE"
-  elif [ "$(cat "$FILE" | md5sum)" != "$(cat "$FILE.new" | md5sum)" ]
-  then
-    #     |--------|--------------------------------------------------|
-    echo "WARNING: $FILE has been customised."
-    echo "         Examine the $FILE.new file"
-    echo "         and integrate any changes into the custom file."
-    echo
-  else
-    rm -f "$FILE.new"
+# Preserve new configuration files
+install_file() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
   fi
+  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 install_file etc/gre.d/mozilla-firefox.conf.new
 
+# update desktop entries
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
+# build search index for mono
 if [ -x usr/bin/monodoc ]; then
-  echo "Generating monodoc search index."
   chroot . usr/bin/monodoc --make-index 1> /dev/null 2> /dev/null
 fi
