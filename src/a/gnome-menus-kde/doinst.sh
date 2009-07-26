ad33d3a9795b6c2bf088e8e67f221415fe63f5a5
Steve Kennedy <steve@gnomeslackbuild.org>
2009-07-26 19:46:11 +0000

    * updates to a set * updates to l set * upgrade mozilla-xulrunner to 1.9.0.12 * add license to mono SlackBuild

--- src/a/gnome-menus-kde/doinst.sh
+++ src/a/gnome-menus-kde/doinst.sh
@@ -1,8 +1,8 @@
-# Icon cache refresh
+# icon cache update
 for i in gnome hicolor
 do
-        if [ -e usr/share/icons/$i/icon-theme.cache ]; then
-                rm -f usr/share/icons/$i/icon-theme.cache
-        fi
-        usr/bin/gtk-update-icon-cache -f -q usr/share/icons/$i
+  if [ -e usr/share/icons/$i/icon-theme.cache ]; then
+    rm -f usr/share/icons/$i/icon-theme.cache
+  fi
+  usr/bin/gtk-update-icon-cache -f -q usr/share/icons/$i 1>/dev/null 2>/dev/null
 done
--- src/a/gnome-themes/doinst.sh
+++ src/a/gnome-themes/doinst.sh
@@ -1,7 +1,7 @@
+# clean up old icon caches
 for i in HighContrast-SVG Crux Mist HighContrast LargePrint HighContrastLargePrint ;
 do
     if [ -e usr/share/icons/$i/icon-theme.cache ]; then
         rm -f usr/share/icons/$i/icon-theme.cache
     fi
-#usr/bin/gtk-update-icon-cache -q -f --ignore-theme-index usr/share/icons/$i
 done
