956065a93e4bfce17cd8535f16c8b2b5b4248a16
Steve Kennedy <steve@gnomeslackbuild.org>
2009-08-13 08:11:22 +0000

    * last changes to pasture

--- pasture/grip/doinst.sh
+++ pasture/grip/doinst.sh
@@ -1,7 +1,4 @@
-if [ -x usr/bin/rarian-sk-update ]; then
-  usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
-fi
-
+# update desktop entries
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
