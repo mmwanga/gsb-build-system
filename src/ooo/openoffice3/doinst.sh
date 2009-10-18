f6b2f9b5a505b80b0546479718ecdfbf10c8ebde
Steve Kennedy <steve@gnomeslackbuild.org>
2009-10-18 21:05:47 +0000

    * update ooo to 3.1.1.3

--- src/ooo/openoffice3/doinst.sh
+++ src/ooo/openoffice3/doinst.sh
@@ -13,6 +13,17 @@ install_file() {
 
 install_file etc/bash_completion.d/ooffice3.1.sh.new
 
+# Install dictionary extensions for spellchecking
+if [ -x "usr/lib@LIBDIRSUFFIX@/ooo-3.1/program/unopkg" ]; then
+  UNOPKGTMP=$TMP/$$$$$$
+  if [ -f "usr/lib@LIBDIRSUFFIX@/ooo-3.1/share/extension/install/dict-@LANG@.oxt" ]; then
+    usr/lib@LIBDIRSUFFIX@/ooo-3.1/program/unopkg add --shared --shared \
+        usr/lib@LIBDIRSUFFIX@/ooo-3.1/share/extension/install/dict-@LANG@.oxt \
+        "-env:UserInstallation=file:///$UNOPKGTMP" 2>/dev/null 1>/dev/null
+  fi;
+  rm -rf $UNOPKGTMP
+fi
+
 # update desktop entries
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database -q usr/share/applications
