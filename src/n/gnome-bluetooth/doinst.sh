b3e5878645b87570d674b12333cd8ba24c2b1e19
fidesratio <steve@gnomeslackbuild.org>
2010-05-03 21:41:32 +0100

    Upgrade gnome-bluetooth to 2.30.0.

--- src/n/gnome-bluetooth/doinst.sh
+++ src/n/gnome-bluetooth/doinst.sh
@@ -8,6 +8,11 @@ if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
+# update mime database
+if [ -x usr/bin/update-mime-database ]; then
+  usr/bin/update-mime-database usr/share/mime 1> /dev/null 2> /dev/null
+fi;
+
 # update hicolor icons
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
         rm -f usr/share/icons/hicolor/icon-theme.cache
