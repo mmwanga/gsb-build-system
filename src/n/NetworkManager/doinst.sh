96f78dc870fce290455fd9ef0e9769fcd614864c
fidesratio <steve@gnomeslackbuild.org>
2010-05-08 12:46:29 +0100

    Upgrade Networkmanager to 0.8.

--- src/n/NetworkManager/doinst.sh
+++ src/n/NetworkManager/doinst.sh
@@ -15,7 +15,7 @@ install_file etc/rc.d/rc.networkmanager.new
 install_file etc/NetworkManager/nm-system-settings.conf.new
 install_file etc/dbus-1/system.d/nm-avahi-autoipd.conf.new
 install_file etc/dbus-1/system.d/NetworkManager.conf.new
-install_file etc/dbus-1/system.d/nm-system-settings.conf.new
+#install_file etc/dbus-1/system.d/nm-system-settings.conf.new
 install_file etc/dbus-1/system.d/nm-dhcp-client.conf.new
 install_file etc/dbus-1/system.d/nm-dispatcher.conf.new
 
