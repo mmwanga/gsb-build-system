e5bb174b184f48842ea6e34c79c05c96dd62e431
fidesratio <steve@gnomeslackbuild.org>
2010-05-10 15:22:12 +0100

    For xinitrc.gnome in gnome-session. Remove splash.

--- src/a/gnome-session/doinst.sh
+++ src/a/gnome-session/doinst.sh
@@ -1,37 +1,16 @@
-# preserve files
-install_file() {
-  NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
+# Remove old .new xinitrc
+rm $PKG/etc/X11/xinit/xinitrc.gnome.new
 
-install_file etc/X11/xinit/xinitrc.gnome.new
-
-# update desktop entries
+# Update desktop entries
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
-# Set GSB splash screen as default
-GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" usr/bin/gconftool-2 --direct --type string \
-  --set /apps/gnome-session/options/splash_image splash/gsb-splash.png 1> /dev/null 2> /dev/null
-# Ensure sound server start up
-GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" usr/bin/gconftool-2 --direct --type boolean \
-  --set /desktop/gnome/sound/enable_esd false 1> /dev/null 2> /dev/null
-# Enable splash screen
-GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" usr/bin/gconftool-2 --direct --type boolean \
-  --set /apps/gnome-session/options/show_splash_screen true 1> /dev/null 2> /dev/null
 # Enable gnome-wm as windowizer
 GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" usr/bin/gconftool-2 --direct --type string \
   --set /desktop/gnome/session/required_components/windowmanager gnome-wm 1> /dev/null 2> /dev/null
 
-# update icons
+# Update icons
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
 	rm -f usr/share/icons/hicolor/icon-theme.cache
 fi
