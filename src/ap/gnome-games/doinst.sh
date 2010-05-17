842be3650cae8132c804443e4fcf869f29949b24
fidesratio <steve@gnomeslackbuild.org>
2010-05-17 15:25:25 +0100

    Upgrade gnome-games to 2.30.1.

--- src/ap/gnome-games/doinst.sh
+++ src/ap/gnome-games/doinst.sh
@@ -1,18 +1,3 @@
-# Preserve new configuration files
-install_file() {
-  NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-install_file etc/ggz.modules.new
-
 # update rarian database
 if [ -x usr/bin/rarian-sk-update ]; then
   usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
