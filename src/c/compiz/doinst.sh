f49ff37ed9c189d886628ef3668ed194f3fa2fbf
fidesratio <steve@gnomeslackbuild.org>
2010-05-24 16:42:34 +0100

    Upgrade compiz to 0.8.6.

--- src/c/compiz/doinst.sh
+++ src/c/compiz/doinst.sh
@@ -1,18 +1,3 @@
-# Preserve new configuration files
-install_file() {
-  NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-install_file etc/X11/xinit/xinitrc.compiz-gnome.new
-
 # update desktop entries
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
