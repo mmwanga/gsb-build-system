ea2953f9ab898b1512cf7adc37b6a345e438fdab
fidesratio <steve@gnomeslackbuild.org>
2010-05-26 11:14:12 +0100

    Remove obsolete packages from a/ set.

--- src/a/gnome-menus-xfce/doinst.sh
+++
@@ -1,8 +0,0 @@
-# refresh icon cache for gnome and hicolor
-for i in gnome hicolor
-do
-        if [ -e usr/share/icons/$i/icon-theme.cache ]; then
-                rm -f usr/share/icons/$i/icon-theme.cache
-        fi
-        usr/bin/gtk-update-icon-cache -f -q usr/share/icons/$i
-done
