e3900a88addad513cf6ee6e07765edddb480de00
fidesratio <steve@gnomeslackbuild.org>
2010-05-26 11:52:29 +0100

    Remove old stuff from n/ set.

--- src/n/gnome-phone-manager/doinst.sh
+++
@@ -1,9 +0,0 @@
-# update desktop entries
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
-fi
-
-# Restart gconfd-2 if running to reload new gconf settings
-if ps acx | grep -q gconfd-2 ; then
-        killall -HUP gconfd-2 ;
-fi
