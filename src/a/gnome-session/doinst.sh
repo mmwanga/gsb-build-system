045cedd4043689631f18d1e78caa5705c4a8bdee
fidesratio <steve@gnomeslackbuild.org>
2010-05-31 11:38:23 +0100

    Add splash to gnome-session, but disable by default.

--- src/a/gnome-session/doinst.sh
+++ src/a/gnome-session/doinst.sh
@@ -1,4 +1,4 @@
-# Remove old .new xinitrc
+# Remove old .new xinitrc if it exists
 rm -f $PKG/etc/X11/xinit/xinitrc.gnome.new
 
 # Update desktop entries
@@ -9,6 +9,14 @@ fi
 # Enable gnome-wm as windowizer
 GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" usr/bin/gconftool-2 --direct --type string \
   --set /desktop/gnome/session/required_components/windowmanager gnome-wm 1> /dev/null 2> /dev/null
+
+# Set GSB splash screen as default
+GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" usr/bin/gconftool-2 --direct --type string \
+  --set /apps/gnome-session/options/splash_image splash/gsb-splash.png 1> /dev/null 2> /dev/null
+
+# Disable splash screen by default
+GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" usr/bin/gconftool-2 --direct --type boolean \
+  --set /apps/gnome-session/options/show_splash_screen false 1> /dev/null 2> /dev/null
 
 # Update icons
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
