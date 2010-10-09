6083dc73e353848a11ed9ef6e899199df76ab071
fidesratio <steve@gnomeslackbuild.org>
2010-10-09 19:18:23 +0100

    upgrade g-d-s to 2.32.0.
    This version now builds against polkit for the clock mechanism.

--- src/a/gnome-settings-daemon/doinst.sh
+++ src/a/gnome-settings-daemon/doinst.sh
@@ -1,3 +1,19 @@
+# Preserve new configuration files
+install_file() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+# Only needed for polkit
+install_file etc/dbus-1/system.d/org.gnome.SettingsDaemon.DateTimeMechanism.conf.new
+
 # update hicolor icons
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
         rm -f usr/share/icons/hicolor/icon-theme.cache
