c597289661d26eb18b3913702588225b6cb755f8
fidesratio <steve@gnomeslackbuild.org>
2010-10-16 11:20:43 +0100

    disable gnomebreakpad in bug-buddy.
    The gconf string /apps/gnome_settings_daemon/gtk-modules/gnomebreakpad
    should be set to false since the the gnomebreakpad module has been
    changed to gnomesegvhandler.  This causes ugly warnings of missing
    modules (though they are harmless).

--- src/ap/bug-buddy/doinst.sh
+++ src/ap/bug-buddy/doinst.sh
@@ -9,6 +9,12 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
 fi
 usr/bin/gtk-update-icon-cache -f -q usr/share/icons/hicolor 1>/dev/null 2>/dev/null
 
+if [ -x usr/bin/gconftool-2 ]; then
+  # breakpad has been removed from the latest bug-buddy and has been replaced 
+  # with gnomesegvhandler.
+  usr/bin/gconftool-2 --direct --config-source="$(usr/bin/gconftool-2 --get-default-source)" --type boolean --set /apps/gnome_settings_daemon/gtk-modules/gnomebreakpad false 1> /dev/null 2> /dev/null
+fi;
+
 # Restart gconfd-2 if running to reload new gconf settings
 if ps acx | grep -q gconfd-2 ; then
         killall -HUP gconfd-2 ;
