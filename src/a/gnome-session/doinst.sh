0cf0fc1987e1d975720c67fdf185c03e2d81bbf2
fidesratio <steve@gnomeslackbuild.org>
2011-02-06 21:26:29 +0000

    upgrade gnome-session to 2.91.

--- src/a/gnome-session/doinst.sh
+++ src/a/gnome-session/doinst.sh
@@ -6,22 +6,13 @@ if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
 fi
 
-# Enable gnome-wm as windowizer
-usr/bin/gconftool-2 --direct --config-source="$(usr/bin/gconftool-2 --get-default-source)" --type string --set /desktop/gnome/session/required_components/windowmanager gnome-wm 1> /dev/null 2> /dev/null 
-
-# Set GSB splash screen as default
-usr/bin/gconftool-2 --direct --config-source="$(usr/bin/gconftool-2 --get-default-source)" --type string --set /apps/gnome-session/options/splash_image splash/gnome-splash.png 1> /dev/null 2> /dev/null
-
-# Disable splash screen by default
-usr/bin/gconftool-2 --direct --config-source="$(usr/bin/gconftool-2 --get-default-source)" --type boolean --set /apps/gnome-session/options/show_splash_screen false 1> /dev/null 2> /dev/null
-
 # Update icons
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
 	rm -f usr/share/icons/hicolor/icon-theme.cache
 fi
 usr/bin/gtk-update-icon-cache -f -q usr/share/icons/hicolor
 
-# Restart gconfd-2 if running to reload new gconf settings
-if ps acx | grep -q gconfd-2 ; then
-        killall -HUP gconfd-2 ;
+# glib2 compile FileChooser schema
+if [ -x usr/bin/glib-compile-schemas ]; then
+  usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas
 fi
