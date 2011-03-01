876fc7af67adea2bc58735daabb051615c8f8a78
fidesratio <steve@gnomeslackbuild.org>
2011-03-01 00:10:58 +0000

    update a/ set for gsb-3.0.
    update slack-descs, SlackBuilds, and change doinst.sh -> doinst.sh.gz.

--- src/a/gvfs/doinst.sh
+++ src/a/gvfs/doinst.sh
@@ -12,8 +12,3 @@ config() {
 }
 
 config etc/profile.d/gvfs-bash-completion.sh.new
-
-# Restart gconfd-2 if running to reload new gconf settings
-if ps acx | grep -q gconfd-2 ; then
-        killall -HUP gconfd-2 ;
-fi
--- src/a/libgnomekbd/doinst.sh
+++
@@ -1,9 +0,0 @@
-# update desktop entries
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
-fi
-
-# glib2 compile schema
-if [ -x usr/bin/glib-compile-schemas ]; then
-  usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas
-fi
