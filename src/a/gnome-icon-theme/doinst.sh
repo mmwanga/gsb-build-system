700e8228966af220762d3353c9a6a81c2262fb42
fidesratio <steve@gnomeslackbuild.org>
2011-03-08 21:29:14 +0000

    upgrade a/ to 2.91.91.

--- src/a/gnome-icon-theme/doinst.sh
+++
@@ -1,5 +0,0 @@
-# update gnome icons
-if [ -e usr/share/icons/gnome/icon-theme.cache ]; then
-        rm -f usr/share/icons/gnome/icon-theme.cache
-fi
-usr/bin/gtk-update-icon-cache -f -q usr/share/icons/gnome 2>/dev/null 1>/dev/null
