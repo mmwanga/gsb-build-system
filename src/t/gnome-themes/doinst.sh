e2750558585cac7bd28cb505c5c7b8e3f7eb003a
fidesratio <steve@gnomeslackbuild.org>
2011-03-19 17:06:15 +0000

    upgrade gnome-themes.

--- src/t/gnome-themes/doinst.sh
+++
@@ -1,7 +0,0 @@
-# clean up old icon caches
-for i in HighContrast-SVG Crux Mist HighContrast LargePrint HighContrastLargePrint ;
-do
-    if [ -e usr/share/icons/$i/icon-theme.cache ]; then
-        rm -f usr/share/icons/$i/icon-theme.cache
-    fi
-done
