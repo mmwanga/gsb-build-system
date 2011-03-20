cec836052b6667c153d14677a120744211716a59
fidesratio <steve@gnomeslackbuild.org>
2011-03-20 16:14:41 +0000

    upgrade gst-python

--- src/l/gst-python/doinst.sh
+++
@@ -1,9 +0,0 @@
-# update mime database
-if [ -x /usr/bin/update-mime-database ]; then
-  /usr/bin/update-mime-database /usr/share/mime 2> /dev/null 1> /dev/null
-fi
-
-# update desktop entries
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database 2> /dev/null 1> /dev/null
-fi
