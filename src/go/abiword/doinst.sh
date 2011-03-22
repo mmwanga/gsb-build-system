5ad5b12d603abe4ea4cec3834962a35442626f16
fidesratio <steve@gnomeslackbuild.org>
2011-03-22 21:32:45 +0000

    upgrade abiword

--- src/go/abiword/doinst.sh
+++
@@ -1,9 +0,0 @@
-# update desktop entries
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
-fi
-
-# update mime databases
-if [ -x usr/bin/update-mime-database ]; then
-  usr/bin/update-mime-database usr/share/mime 1> /dev/null 2> /dev/null
-fi
