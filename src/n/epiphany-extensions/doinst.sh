08eb7d1d82b935c32885010b61f87c219b993175
fidesratio <steve@gnomeslackbuild.org>
2011-04-03 11:10:44 +0000

    update n/ set.
    upgrade epiphany-extensions; gnome-nettool;  papyon;  and
    telepathy plugins like gabble and salut.

--- src/n/epiphany-extensions/doinst.sh
+++
@@ -1,9 +0,0 @@
-# update rarian database
-if [ -x usr/bin/rarian-sk-update ]; then
-  usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
-fi
-
-# Restart gconfd-2 if running to reload new gconf settings
-if ps acx | grep -q gconfd-2 ; then
-        killall -HUP gconfd-2 ;
-fi
