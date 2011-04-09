c6081451ca0f353d703456ddf0464ebbe29383f7
fidesratio <steve@gnomeslackbuild.org>
2011-04-09 11:54:07 +0000

    bring tree up-to-date for GSB 3.0

--- extra/balsa/doinst.sh
+++
@@ -1,9 +0,0 @@
-# update rarian database
-if [ -x usr/bin/rarian-sk-update ]; then
-  usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
-fi
-
-# update desktop database
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
-fi
